b618a05c3c93c394e6faea21d9a3d14fb4107086
Gabriel Marin <gabriel.marin-isaza@capgemini.com>
2020-11-03 21:26:53 -0500

    initial config

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld :msg="title"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  setup () {
+    const title = ref('hola')
+    const newTitle = () => {
+      title.value = 'hello word'
+    }
+    newTitle()
+    return { title, newTitle }
+  }
+}
+</script>
---
+++ frontend/src/views/layouts/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
